5dfd96041d1430268eabb776326c2835d5eb3267
taskin8651 <mdsayebalam10@gmail.com>
2026-02-21 15:32:22 +0530

    xcx

---
+++ app/Models/VideoReview.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class VideoReview extends Model
+{
+    use HasFactory;
+}
