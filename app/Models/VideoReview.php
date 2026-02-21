5cabdd5e2345372663105454b0f8592c547233a3
taskin8651 <mdsayebalam10@gmail.com>
2026-02-21 15:54:36 +0530

    sd

--- app/Models/VideoReview.php
+++ app/Models/VideoReview.php
@@ -2,10 +2,27 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class VideoReview extends Model
 {
-    use HasFactory;
+    protected $fillable = [
+        'name',
+        'designation',
+        'youtube_url',
+        'thumbnail',
+        'is_active',
+    ];
+
+    // Extract YouTube ID
+    public function getYoutubeIdAttribute()
+    {
+        preg_match(
+            '/(?:youtube\.com\/(?:watch\?v=|embed\/)|youtu\.be\/)([^\&\?\/]+)/',
+            $this->youtube_url,
+            $matches
+        );
+
+        return $matches[1] ?? null;
+    }
 }
