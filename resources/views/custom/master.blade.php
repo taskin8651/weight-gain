6d4cfc5542f225837a836272fb1fe1281e539fe0
taskin8651 <mdsayebalam10@gmail.com>
2026-03-14 12:27:30 +0530

    bnn

--- resources/views/custom/master.blade.php
+++ resources/views/custom/master.blade.php
@@ -32,12 +32,12 @@ $settings = App\Models\Setting::first();
     </head>
     <body>
         <!-- Preloader -->
-        <div class="preloader">
+        <!-- <div class="preloader">
             <div class="spinner">
                 <div class="dot1"></div>
                 <div class="dot2"></div>
             </div>
-        </div>
+        </div> -->
         <!-- Preloader End -->
 
         <!-- Top Header Start -->
@@ -317,23 +317,7 @@ $settings = App\Models\Setting::first();
         <!-- Footer Area End -->
 
         <!-- Modal Start -->
-        <div class="modal fade fade-scale searchmodal" id="searchmodal" tabindex="-1" role="dialog">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <button type="button" class="close" data-bs-dismiss="modal">
-                            <i class='bx bx-x'></i>
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                        <form class="modal-search-form">
-                            <input type="search" class="search-field" placeholder="Search...">
-                            <button type="submit"><i class='bx bx-search-alt'></i></button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
+        
         <!-- Modal End -->
 
         <!-- WhatsApp Floating Button -->
